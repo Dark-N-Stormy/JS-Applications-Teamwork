76029415a91875bf7a61f1be2568f688ff9df9a3
Nikolay Petrov <nikolay.petrov@abv.bg>
2015-09-02 23:30:55 +0300

    first unit test using mocha, chai and sinon

---
+++ tests/units/testChat.js
@@ -0,0 +1,10 @@
+(function() {
+    describe('Chat', function() {
+        it('expect chatHandler to exist', function() {
+            //var chatHandler = new Chat();
+
+
+            expect(1).to.be.equal(1);
+        });
+    });
+}());
